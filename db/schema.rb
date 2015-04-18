a0529744caaef1106ef52f1a30bcd3ae027c6c46
Melanie <murray.melanief@gmail.com>
2015-04-18 16:24:19 -0500

    Added age parameters to app

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418191341) do
+ActiveRecord::Schema.define(version: 20150418211802) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
